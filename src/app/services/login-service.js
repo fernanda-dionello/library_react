30492ab6facf601780e634ba459fc5f431971a52
fernanda-dionello <fernanda.dionello@gmail.com>
2022-07-05 00:17:36 -0300

    fix: add env variables

--- src/app/services/login-service.js
+++ src/app/services/login-service.js
@@ -1,11 +1,11 @@
 import httpService from "./http-service";
-import { REACT_APP_USERNAME, REACT_APP_PASSWORD } from "../../env_variables";
+// import { REACT_APP_USERNAME, REACT_APP_PASSWORD } from "../../env_variables";
 
 const loginService = {
     login: async () => {
         const data = {
-            'username': REACT_APP_USERNAME,
-            'password': REACT_APP_PASSWORD,
+            'username': process.env.REACT_APP_USERNAME,
+            'password': process.env.REACT_APP_PASSWORD,
         }
         return await httpService.post('login', data);
     }
--- src/env_variables.js
+++
@@ -1,3 +0,0 @@
-export const REACT_APP_USERNAME = 'fernanda'
-export const REACT_APP_PASSWORD = 'password123'
-
